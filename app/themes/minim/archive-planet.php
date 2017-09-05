c8fdef8966a828116fd31a36787a39c7dce76d76
Jim <jim@advancebass.com>
2017-09-04 23:52:40 -0700

    adding pod fields

--- app/themes/minim/archive-planet.php
+++ app/themes/minim/archive-planet.php
@@ -20,21 +20,29 @@ get_header();
                     $image = $planets->field('planet_image');
                     $image_url = pods_image_url($image, $size = 'medium', $default = 0, $force = false);
 
+                    $diameter = $planets->field('planet_diameter');
+
                     ?>
 
                     <h2><a href="<?= $permalink; ?>"><?= $planets->display('planet_name'); ?></a></h2>
 
 
-                    <?php if (!empty($image_url)): ?>
+                    <?php
 
+                    if (!empty($image_url)): ?>
                         <img src="<?php echo $image_url; ?>">
                         <?php
+                    endif;
 
+                    if (!empty($diameter)): ?>
+                        <p><?php echo $diameter . ' km'; ?></p>
+
+                        <?php
                     endif;
 
                     ?>
 
-                    <p>Description: <?php echo $planets->display('planet_description'); ?></p>
+                    <p><?php echo $planets->display('planet_description'); ?></p>
 
                     <?php
 

e34209072ebd63aa3f0c06abb9e9375095c6127c
Jim <jim@advancebass.com>
2017-11-01 19:27:51 -0700

    commit before pull, just incase something was useful in the current changes

--- app/themes/atom/functions.php
+++ app/themes/atom/functions.php
@@ -5,7 +5,8 @@
  */
 function minim_enqueue_sripts()
 {
-	wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+  wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+  wp_enqueue_style('custom-style', get_stylesheet_directory_uri()  . '/css/custom.css');
 }
 add_action('wp_enqueue_scripts', 'minim_enqueue_sripts');
 

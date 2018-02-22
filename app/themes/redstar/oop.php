75a86c401eaab5c90797062bb9e2e432a5bc165a
Jim <jim@advancebass.com>
2018-02-21 20:39:24 -0800

    add red-widget and template for oop testing

---
+++ app/themes/redstar/oop.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Template Name: OOP
+ */
+
+class Dog {
+	public $name;
+	public $breed;
+	public $colour;
+	public function bark() {
+		return 'Woof!';
+	}
+	public function walk() {
+		return 'Happy dog :)';
+	}
+}
+

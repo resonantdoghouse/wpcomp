a34a89bc8af8cd201d8832cca43338846f670972
Jim <jim@advancebass.com>
2018-02-26 09:36:12 -0800

    micro refactor and update comments

--- app/themes/atom/functions.php
+++ app/themes/atom/functions.php
@@ -1,29 +1,28 @@
 <?php
 
 /**
- * Child Theme use parent style
+ * Child Theme parent style
  */
+add_action('wp_enqueue_scripts', 'minim_enqueue_sripts');
 function minim_enqueue_sripts()
 {
   wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
   wp_enqueue_style('custom-style', get_stylesheet_directory_uri()  . '/css/custom.css');
 }
-add_action('wp_enqueue_scripts', 'minim_enqueue_sripts');
 
 
 
-
-
-
-
-
-
-
+/**
+ *  Admin a custom menu to the admin page
+ */
 add_action( 'admin_menu', 'linked_url' );
 function linked_url() {
 	add_menu_page( 'linked_url', 'External link', 'read', 'my_slug', '', 'dashicons-text', 1 );
 }
 
+/**
+ * Admin custom menu link to edit a post
+ */
 add_action( 'admin_menu' , 'linkedurl_function' );
 function linkedurl_function() {
 	global $menu;

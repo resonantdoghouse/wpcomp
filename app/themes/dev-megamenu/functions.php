cd2874380bba39fa31a07a8fda9ae59983c369b6
Jim <jim@advancebass.com>
2018-03-14 14:17:45 -0700

    add mega menu with widget content

--- app/themes/dev-megamenu/functions.php
+++ app/themes/dev-megamenu/functions.php
@@ -5,6 +5,8 @@
  */
 add_action( 'wp_enqueue_scripts', 'dev_megamenu_enqueue_scripts' );
 add_action( 'after_setup_theme', 'dev_megamenu_register_menu' );
+add_action( 'widgets_init', 'dev_megamenu_widgets_init' );
+
 
 /**
  * Child Theme parent style & custom stylesheet
@@ -24,7 +26,28 @@ function dev_megamenu_enqueue_scripts() {
 }
 
 
-
 function dev_megamenu_register_menu() {
-	register_nav_menu( 'dev-megamenu', __( 'Mega Menu!', 'dev-megamenu' ) );
+	register_nav_menus( array(
+		'dev_megamenu' => 'Mega Menu!'
+	) );
+}
+
+
+function dev_megamenu_widgets_init() {
+	$location  = 'dev_megamenu';
+	$css_class = 'has-mega-menu';
+	$locations = get_nav_menu_locations();
+	if ( isset( $locations[ $location ] ) ) {
+		$menu = get_term( $locations[ $location ], 'nav_menu' );
+		if ( $items = wp_get_nav_menu_items( $menu->name ) ) {
+			foreach ( $items as $item ) {
+				if ( in_array( $css_class, $item->classes ) ) {
+					register_sidebar( array(
+						'id'   => 'mega-menu-widget-area-' . $item->ID,
+						'name' => $item->title . ' - Mega Menu',
+					) );
+				}
+			}
+		}
+	}
 }
--- app/themes/dev-megamenu/header.php
+++ app/themes/dev-megamenu/header.php
@@ -27,7 +27,9 @@
         <a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentysixteen' ); ?></a>
 
         <header id="masthead" class="site-header" role="banner">
+
             <div class="site-header-main">
+
                 <div class="site-branding">
 					<?php twentysixteen_the_custom_logo(); ?>
 
@@ -46,172 +48,36 @@
                 </div><!-- .site-branding -->
 
 
+                <!--
+                /**
+                 * Mega Menu
+                 *
+                 */
+                 -->
+                <ul class="mega-menu">
+					<?php
+					$locations = get_nav_menu_locations();
+					if ( isset( $locations['dev_megamenu'] ) ) {
+						$menu = get_term( $locations['dev_megamenu'], 'nav_menu' );
+						if ( $items = wp_get_nav_menu_items( $menu->name ) ) {
+							foreach ( $items as $item ) {
+								echo "<li class=\"mega-menu__item\">";
+								echo "<a href=\"{$item->url}\">{$item->title}</a>";
+								if ( is_active_sidebar( 'mega-menu-widget-area-' . $item->ID ) ) {
+									echo "<ul id=\"mega-menu-{$item->ID}\" class=\"mega-menu__item__content\">";
+									dynamic_sidebar( 'mega-menu-widget-area-' . $item->ID );
+									echo "</ul>";
+								}
+								echo "</li>";
+							}
+						}
+					}
+					?>
+                </ul>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                <!-- NAV BAR -->
-                <div class="primary-menu__wrapper">
-                    <div class="primary-menu">
-                        <nav class="nav" role="navigation">
-                            <ul>
-								<?php $locations = get_nav_menu_locations();
-
-								// if there's a location for the primary menu
-								if ( isset( $locations['dev-megamenu'] ) ) {
-									$menu = get_term( $locations['dev-megamenu'], 'nav_menu' );
-
-									// if there are items in the primary menu
-									if ( $items = wp_get_nav_menu_items( $menu->name ) ) {
-
-										// loop through all menu items to display their content
-										foreach ( $items as $item ) {
-
-											// if the current item is not a top level item, skip it
-											if ( $item->menu_item_parent != 0 ) {
-												continue;
-											}
-
-											// get the ID of the current nav item
-											$curNavItemID = $item->ID;
-
-											// get the custom classes for the item
-											// (determined within the WordPress Appearance > Menu section)
-											$classList = implode( " ", $item->classes );
-											echo "<li class=\"{$classList}\">";
-											echo "<a href=\"{$item->url}\">{$item->title}</a>";
-
-											// build the mega-menu
-											// if 'mega-menu'  exists within the class
-											if ( in_array( 'has-mega-menu', $item->classes ) ) { ?>
-                                                <div class="mega-menu__wrapper js-mega-menu">
-                                                    <div class="mega-menu">
-
-                                                        <div class="mega-menu__content">
-                                                            <h2><?= $item->post_title; ?></h2>
-                                                            <p><?= $item->description; ?></p>
-                                                            <a href="<?= $item->url; ?>" class="learn-more">Learn
-                                                                More</a>
-                                                        </div>
-
-                                                        <div class="mega-menu__subnav">
-                                                            <nav>
-                                                                <ul class="subnav">
-																	<?php // cycle through the menu items and get the subnav
-																	foreach ( $items as $subnav ) {
-																		if ( $subnav->menu_item_parent == $curNavItemID ) {
-																			echo "<li><a href=\"{$subnav->url}\">{$subnav->title}</a>";
-																		}
-																	} ?>
-                                                                </ul>
-                                                            </nav>
-                                                        </div>
-
-
-                                                    </div>
-                                                </div>
-											<?php }
-
-											// if this is the search bar
-											if ( in_array( 'nav-search', $item->classes ) ) { ?>
-                                                <div class="search-bar__wrapper">
-                                                    <div class="search-bar">
-                                                        <div class="search-bar__form">
-                                                            <form>
-                                                                <input type="text" name="s" placeholder="Keywords"/>
-                                                                <button type="button" role="submit" name="Search">
-                                                                    Search
-                                                                </button>
-                                                            </form>
-                                                            <a href="#" class="js-close-search search-bar__close">
-                                                                <svg role="img" class="icon">
-                                                                    <use xlink:href="<?php bloginfo( 'template_url' ); ?>/assets/dist/img/svg.svg#close"></use>
-                                                                </svg>
-                                                            </a>
-                                                        </div> <!-- /.search-bar__form -->
-                                                    </div><!-- /.search-bar -->
-                                                </div><!-- /.search-bar__wrapper -->
-											<?php }
-											echo '</li>';
-
-										}
-									}
-								} ?>
-                            </ul>
-							<?php //nationsu_primary_nav(); ?>
-                        </nav>
-                    </div><!-- /.primary-menu -->
-                </div> <!-- /.primary-menu__wrapper -->
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-				<?php if ( has_nav_menu( 'primary' ) || has_nav_menu( 'social' ) ) : ?>
-                    <button id="menu-toggle" class="menu-toggle"><?php _e( 'Menu', 'twentysixteen' ); ?></button>
-
-                    <div id="site-header-menu" class="site-header-menu">
-
-						<?php if ( has_nav_menu( 'primary' ) ) : ?>
-                            <nav id="site-navigation" class="main-navigation" role="navigation"
-                                 aria-label="<?php esc_attr_e( 'Primary Menu', 'twentysixteen' ); ?>">
-								<?php
-								wp_nav_menu( array(
-									'theme_location' => 'primary',
-									'menu_class'     => 'primary-menu',
-								) );
-								?>
-                            </nav><!-- .main-navigation -->
-						<?php endif; ?>
-
-						<?php if ( has_nav_menu( 'social' ) ) : ?>
-                            <nav id="social-navigation" class="social-navigation" role="navigation"
-                                 aria-label="<?php esc_attr_e( 'Social Links Menu', 'twentysixteen' ); ?>">
-								<?php
-								wp_nav_menu( array(
-									'theme_location' => 'social',
-									'menu_class'     => 'social-links-menu',
-									'depth'          => 1,
-									'link_before'    => '<span class="screen-reader-text">',
-									'link_after'     => '</span>',
-								) );
-								?>
-                            </nav><!-- .social-navigation -->
-						<?php endif; ?>
-                    </div><!-- .site-header-menu -->
-				<?php endif; ?>
             </div><!-- .site-header-main -->
+
 
 			<?php if ( get_header_image() ) : ?>
 				<?php
